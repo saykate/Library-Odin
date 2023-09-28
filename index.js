cbbc490550d533dd4866119c3e2740aed7089d91
Katie Sayers <kate@student.dev-island.org>
2023-09-28 12:17:53 -1000

    cleaned up dom elements. got read button working. still can't get delete button and lack of object

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const addBook = document.querySelector('.add-book');
-const readStatus = document.querySelectorAll('.read-status');
 const deleteBk = document.querySelectorAll('.delete');
 const readNum = document.querySelector('.read-num');
 const unreadNum = document.querySelector('.unread-num');
@@ -13,46 +12,46 @@ let form = document.querySelector('form'); //should this be let or const?
 let readCount = 0;
 let unreadCount = 0;
 
+//Update the Read books count
 function updateReadCount() {
-    console.log(readCount)
     readNum.textContent = readCount;
 }
 
+//Update the Unread books count
 function updateUnreadCount() {
-    console.log(unreadCount)
     unreadNum.textContent = unreadCount;
 }
 
 //create a book card for each book
-function makeBookCard() {
-    library.forEach((book) => {
+function makeBookCard(newBook) {
         const bookCard = document.createElement('div');
-        bookCard.classList.add('card');
         const hTwo = document.createElement('h2');
-        hTwo.textContent = book.title;
         const para1 = document.createElement('p');
-        para1.textContent = 'By:  ' + book.author;
-        para1.classList.add('para')
         const para2 = document.createElement('p');
-        para2.textContent = 'Pages:  ' + book.pages;
-        para2.classList.add('para')
         const bookButtons = document.createElement('div');
-        bookButtons.classList.add('card-buttons');
         const readAndCheck = document.createElement('div');
-        readAndCheck.classList.add('read-check');
         const readButton = document.createElement('button');
-        readButton.textContent = 'Read';
-        readButton.classList.add('read-status');
-        readButton.classList.add('unread');
         const readCheck = document.createElement('img');
-        readCheck.classList.add('checkmark');
-        readCheck.setAttribute('src', './images/check-bold.svg');
         const deleteButt = document.createElement('button');
-        deleteButt.classList.add('delete');
         const trash = document.createElement('img');
+
+        bookCard.classList.add('card');
+        para1.classList.add('para')
+        para2.classList.add('para')
+        bookButtons.classList.add('card-buttons');
+        readAndCheck.classList.add('read-check');
+        readButton.classList.add('read-status', 'unread');
+        readCheck.classList.add('checkmark');
+        deleteButt.classList.add('delete');
         trash.classList.add('trash');
+
+        hTwo.textContent = newBook.title;
+        para1.textContent = 'By:  ' + newBook.author;
+        para2.textContent = 'Pages:  ' + newBook.pages;
+        readButton.textContent = 'Read';
+
+        readCheck.setAttribute('src', './images/check-bold.svg');
         trash.setAttribute('src', './images/trash-can-outline.svg');
-        deleteButt.appendChild(trash);
 
         cardContainer.appendChild(bookCard);
         bookCard.appendChild(hTwo);
@@ -63,10 +62,47 @@ function makeBookCard() {
         readAndCheck.appendChild(readButton);
         readAndCheck.appendChild(readCheck);
         bookButtons.appendChild(deleteButt);
-    });
+        deleteButt.appendChild(trash);
+
+        //Read button - changes background and adds a check on 'read' cards
+        function readUnread(event) {
+            if(event.target.classList.contains('unread')) {
+            readCheck.style.display = 'block';   
+            bookCard.style.backgroundColor = 'var(--clr-bg-lt-blue)';
+            event.target.classList.remove('unread');
+            event.target.classList.add('read');
+            readCount++;
+            updateReadCount();
+            unreadCount--;
+            updateUnreadCount();
+            } 
+            else if(event.target.classList.contains('read')) {
+            readCheck.style.display = 'none';   
+            bookCard.style.backgroundColor = 'var(--clr-bg-lt)';
+            event.target.classList.remove('read');
+            event.target.classList.add('unread');
+            unreadCount++;
+            updateUnreadCount();
+            readCount--;
+            updateReadCount();
+            } 
+        }
+
+        //Delete book when button is clicked
+        function deleteBook(event) {  
+            console.log('delete')    //this is logging
+            if (event.target.classList.contains('delete')) {
+                bookCard.remove(); //this is not happening - is it because it's still inside the function where it's being created? It doesn't work outside the function either...
+            } //need to update read or unread - how do I find the status? 
+        }
+
+        readButton.addEventListener('click', readUnread);
+        deleteButt.addEventListener('click', deleteBook);   
 }
 
-//add book info from the form input and add it to the library array
+
+
+//Add book info from the form input and add it to the library array
 function addNewBook(e) {
     e.preventDefault();
 
@@ -80,68 +116,15 @@ function addNewBook(e) {
 
     const newBook = {
         title: bookTitle.value, 
-        author: bookAuthor.value, 
+        author: bookAuthor.value,  
         pages: bookPages.value
     }
 
     library.push(newBook);
-    console.log(library);
     form.reset();
-    makeBookCard();
+    makeBookCard(newBook);
     unreadCount++; 
     updateUnreadCount(); 
 }
 
-//Read button - changes background and adds a check on 'read' cards
-function readUnread(event) {
-    const thisCard = event.target.closest('.card');
-
-    if (thisCard) {
-        const thisCheck = thisCard.querySelector('.checkmark');
-
-        if(event.target.classList.contains('unread')) {
-        thisCheck.style.display = 'block';   
-        thisCard.style.backgroundColor = 'var(--clr-bg-lt-blue)';
-        event.target.classList.remove('unread');
-        event.target.classList.add('read');
-        readCount++;
-        updateReadCount();
-        unreadCount--;
-        updateUnreadCount();
-    } 
-        else if(event.target.classList.contains('read')) {
-        thisCheck.style.display = 'none';   
-        thisCard.style.backgroundColor = 'var(--clr-bg-lt)';
-        event.target.classList.remove('read');
-        event.target.classList.add('unread');
-        unreadCount++;
-        updateUnreadCount();
-        readCount--;
-        updateReadCount();
-    } 
-}
-}
-
-//not currently functioning
-//when the .card is being created dynamically, I may need to move this into the function where it's being added (see hackathon project)
-function deleteBook(event) {
-    console.log('delete button clicked')
-    const thisCard = event.target.closest('.card');
-
-    if (thisCard) {
-  if (event.target.classList.contains('delete')) {
-    thisCard.remove();
-  }
-}  
-//need to update read or unread - how do I find the status? 
-  }
-
 addBook.addEventListener('click', addNewBook);
-
-readStatus.forEach(status => {
-    status.addEventListener('click', readUnread);
-})
-
-deleteBk.forEach(delBk => {
-    delBk.addEventListener('click', deleteBook);
-})
