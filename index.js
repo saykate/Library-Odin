eab7eb9ade407a9c78ae0820c7f32d7f1636aa1c
Katie Sayers <kate@student.dev-island.org>
2023-09-27 15:09:14 -1000

    added the addNewBook function and got it working

--- index.js
+++ index.js
@@ -4,19 +4,35 @@ const deleteBk = document.querySelectorAll('.delete');
 const bookTitle = document.querySelector('#title');
 const bookAuthor = document.querySelector('#author');
 const bookPages = document.querySelector('#pages');
-const bookCard = document.querySelectorAll('.card');
-let form = document.querySelector('form')
+// const bookCard = document.querySelectorAll('.card');
+let form = document.querySelector('form');
 
 const library = [];
 
-//can an empty div exist with a button?! How will I add the "By", "Pages", the button?!
-//add inputs to h2, p, p, button. Append them to a div
+
+//add the "By:" & "Pages:" <p>'s and buttons to each card
 //Why do I loop through the array? Am I not creating each one as it's being input?
 
 function addNewBook(e) {
     e.preventDefault();
-    bookInput = new FormData(form);
-    
+    console.log('add book clicked')
+    if (bookTitle.value === '') {
+        return alert('Please fill out Book Title');
+    } else if (bookAuthor.value === '') {
+        return alert('Please fill out Book Author');
+    } else if (bookPages.value === '') {
+        return alert('Please fill out Number of Pages');
+    }
+
+    const newBook = {
+        title: bookTitle.value, 
+        author: bookAuthor.value, 
+        pages: bookPages.value
+    }
+
+    library.push(newBook);
+    console.log(library);
+    form.reset();
 }
 
 function readUnread(event) {
@@ -42,6 +58,7 @@ function readUnread(event) {
 
 //when the .card is being created dynamically, I may need to move this into main or something...
 function deleteBook(event) {
+    console.log('delete button clicked')
     const thisCard = event.target.closest('.card');
 
     if (thisCard) {
